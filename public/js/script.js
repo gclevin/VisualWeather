d326f3660911495b8799d7d6bc165221d1bd49f1
Gabriel Levin <gabriellevin@Gabriels-MacBook-Pro.local>
2018-01-15 15:27:27 -0800

    Enable POST request

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,18 @@
 navigator.geolocation.getCurrentPosition(function(position) {
 
-        console.log("HI");
+        //console.log("HI");
 
         // Get the coordinates of the current position.
-        var lat = position.coords.latitude;
-        var lng = position.coords.longitude;
+        //var lat = position.coords.latitude.toFixed(2);
+        //var lng = position.coords.longitude.toFixed(2);
 
-        console.log(lat);
-        console.log(lng);
+        //Grab coordinates, set them in appropriate field, then send form
+        document.getElementById("lat").value = position.coords.latitude.toFixed(2);
+        document.getElementById("lng").value = position.coords.longitude.toFixed(2);
+        document.getElementById("cords").submit();
+
+
+        //console.log(lat);
+        //console.log(lng);
 });
+
