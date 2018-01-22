9fc43f75849cf591385cd35857140066fc296c68
Gabriel Levin <gabriellevin@Gabriels-MacBook-Pro.local>
2018-01-21 16:20:02 -0800

    Send user coordinates to server and get five day forecast

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const request = require('request');
 const path = require('path');
 const app = express()
 
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+
 app.set('port', process.env.PORT || 8080);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug')
@@ -12,6 +18,9 @@ app.use(express.static(path.join(__dirname, './public')));
 app.listen(app.get('port'), function() {
   console.log('Express started press Ctrl-C to terminate');
 });
+
+let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', "Saturday"];
+
 
 app.get('/', function (req, res) {
   // request({
@@ -26,10 +35,54 @@ app.get('/', function (req, res) {
   //   }
   // });
   console.log('GET');
-  res.render('index', {title: 'Visual Weather', message: 'gabe'});
+  res.render('index', {title: 'Visual Weather', message: 'Loading...'});
 });
 
 app.post('/', function (req, res) {
   console.log('POST');
-  res.render('visual', {title: 'Visual Weather', message: 'POST'});
+  console.log(req.body.lat);
+  console.log(req.body.lng);
+
+  var currentDay = new Date().getDay();
+  console.log(currentDay);
+
+  //let test = "{ 'user': 'gabe' }"
+
+  //let response = "{ Days:\n[ "
+
+  //res.json(test);
+
+
+
+  let link = 'http://api.weatherunlocked.com/api/forecast/' + req.body.lat + ',' + req.body.lng + '?app_id=48cb6b4b&app_key=c71f24d16dfd40152137a56d55e767d2'
+  console.log(link);
+  request({
+    url: link,
+    json: true
+  }, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      let myObj = body
+      let forecast = myObj.Days[1].temp_max_f;
+      console.log(forecast);
+      let day = '';
+
+      // for (let i = 0; i <= 3; i++) {
+      //   day = days[currentDay%7];
+      //   response += "{ day: " + day + ",\n high: " + myObj.Days[currentDay%7].temp_max_f + ",\n low: " + myObj.Days[currentDay%7].temp_min_f + " },\n ";
+      //   currentDay++;
+      // }
+
+      // response += "{ day: " + day + ",\n high: " + myObj.Days[(currentDay+1)%7].temp_max_f + ",\n low: " + myObj.Days[(currentDay+1)%7].temp_min_f + " } ] } ";
+
+      // console.log(response);
+      //res.json(response);
+      res.json({ "temp": "temp" });
+      //res.render('index', {title: 'Visual Weather', message: forecast});
+    }
+  });
+
+
+
+  //res.render('index', {title: 'Visual Weather', message: 'POST'});
 });
+
--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,18 @@
 navigator.geolocation.getCurrentPosition(function(position) {
 
-        //console.log("HI");
-
-        // Get the coordinates of the current position.
-        //var lat = position.coords.latitude.toFixed(2);
-        //var lng = position.coords.longitude.toFixed(2);
-
-        //Grab coordinates, set them in appropriate field, then send form
-        document.getElementById("lat").value = position.coords.latitude.toFixed(2);
-        document.getElementById("lng").value = position.coords.longitude.toFixed(2);
-        document.getElementById("cords").submit();
-
-
-        //console.log(lat);
-        //console.log(lng);
+        fetch('/', {
+  			method: 'POST',
+ 			body: JSON.stringify({ "lat": position.coords.latitude.toFixed(2), "lng": position.coords.longitude.toFixed(2), }),
+ 			headers: new Headers({ 'Content-Type': 'application/json' })
+		}).then(res => {
+			return res.json();
+		}).catch (err => {
+			console.error('Error: ', error);
+		}).then(data => {
+			console.log(data);
+			//var obj = JSON.parse(data);
+			//console.log(obj.Days[0])
+			//console.log(data)
+		});
 });
 
