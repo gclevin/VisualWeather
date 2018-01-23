229d1990d7701eb63af6c629b6aedc02af79e7b3
Gabriel Levin <gabriellevin@Gabriels-MacBook-Pro.local>
2018-01-22 16:40:42 -0800

    Display five day forecast with graph

--- app.js
+++ app.js
@@ -2,38 +2,23 @@ const express = require('express')
 const request = require('request');
 const path = require('path');
 const app = express()
-
-
 const bodyParser = require('body-parser');
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
 
 app.set('port', process.env.PORT || 8080);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug')
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(express.static(path.join(__dirname, './public')));
 
 app.listen(app.get('port'), function() {
   console.log('Express started press Ctrl-C to terminate');
 });
 
-let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', "Saturday"];
 
 
 app.get('/', function (req, res) {
-  // request({
-  //   url: 'http://api.weatherunlocked.com/api/forecast/34.03,-118.39?app_id=48cb6b4b&app_key=c71f24d16dfd40152137a56d55e767d2',
-  //   json: true
-  // }, function (error, response, body) {
-  //   if (!error && response.statusCode === 200) {
-  //     let myObj = body
-  //     let forecast = myObj.Days[0].temp_max_f;
-  //     console.log(body);
-  //     res.render('index', {title: 'Visual Weather', message: forecast});
-  //   }
-  // });
   console.log('GET');
   res.render('index', {title: 'Visual Weather', message: 'Loading...'});
 });
@@ -46,14 +31,6 @@ app.post('/', function (req, res) {
   var currentDay = new Date().getDay();
   console.log(currentDay);
 
-  //let test = "{ 'user': 'gabe' }"
-
-  //let response = "{ Days:\n[ "
-
-  //res.json(test);
-
-
-
   let link = 'http://api.weatherunlocked.com/api/forecast/' + req.body.lat + ',' + req.body.lng + '?app_id=48cb6b4b&app_key=c71f24d16dfd40152137a56d55e767d2'
   console.log(link);
   request({
@@ -62,27 +39,9 @@ app.post('/', function (req, res) {
   }, function (error, response, body) {
     if (!error && response.statusCode === 200) {
       let myObj = body
-      let forecast = myObj.Days[1].temp_max_f;
-      console.log(forecast);
-      let day = '';
-
-      // for (let i = 0; i <= 3; i++) {
-      //   day = days[currentDay%7];
-      //   response += "{ day: " + day + ",\n high: " + myObj.Days[currentDay%7].temp_max_f + ",\n low: " + myObj.Days[currentDay%7].temp_min_f + " },\n ";
-      //   currentDay++;
-      // }
-
-      // response += "{ day: " + day + ",\n high: " + myObj.Days[(currentDay+1)%7].temp_max_f + ",\n low: " + myObj.Days[(currentDay+1)%7].temp_min_f + " } ] } ";
-
-      // console.log(response);
-      //res.json(response);
-      res.json({ "temp": "temp" });
-      //res.render('index', {title: 'Visual Weather', message: forecast});
+      res.json(myObj);
     }
   });
-
-
-
   //res.render('index', {title: 'Visual Weather', message: 'POST'});
 });
 
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,8 @@
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', "Saturday"];
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
 navigator.geolocation.getCurrentPosition(function(position) {
 
         fetch('/', {
@@ -10,9 +15,32 @@ navigator.geolocation.getCurrentPosition(function(position) {
 			console.error('Error: ', error);
 		}).then(data => {
 			console.log(data);
-			//var obj = JSON.parse(data);
-			//console.log(obj.Days[0])
-			//console.log(data)
+			console.log(data.Days[1].temp_max_f);
+
+			let xCord = 10;
+
+			// ctx.fillStyle = "#c1666b";
+			// ctx.fillRect(10, 10, 100, data.Days[1].temp_max_f);
+			
+			// ctx.fillStyle = "#6290c3";
+			// ctx.fillRect(10, 10, 100, data.Days[1].temp_min_f);
+
+			var currentDay = new Date().getDay();
+			ctx.setTransform(1,0,0,-1,0,canvas.height);
+			document.querySelector("h1").innerHTML = "Five Day Forecast";
+
+			for (i = 1; i <= 5; i++) {
+
+				ctx.fillStyle = "#fadf63";
+				ctx.fillRect(xCord, 10, 100, data.Days[i].temp_max_f * 6);
+			
+				ctx.fillStyle = "#035bbf";
+				ctx.fillRect(xCord, 10, 100, data.Days[i].temp_min_f * 6);
+
+				console.log("On " + days[currentDay%7] + " the high will be " + data.Days[i].temp_max_f + " and the low will be " + data.Days[i].temp_min_f + ".");
+				currentDay++;
+				xCord += 102;
+			}
 		});
 });
 
