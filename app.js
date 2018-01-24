d87a103e4ef302e9b47798d8a6ba2e29346d1815
Gabriel Levin <gabriellevin@Gabriels-MacBook-Pro.local>
2018-01-24 09:10:46 -0800

    Add comments

--- app.js
+++ app.js
@@ -6,43 +6,35 @@ const bodyParser = require('body-parser');
 
 app.set('port', process.env.PORT || 8080);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'pug')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(express.static(path.join(__dirname, './public')));
 
+//Start application on local host
 app.listen(app.get('port'), function() {
   console.log('Express started press Ctrl-C to terminate');
 });
 
-
-
 app.get('/', function (req, res) {
   console.log('GET');
   res.sendFile(path.join(__dirname+'/views/index.html'));
-  //res.render('index', {title: 'Visual Weather', message: 'Loading...'});
 });
 
 app.post('/', function (req, res) {
-  console.log('POST');
-  console.log(req.body.lat);
-  console.log(req.body.lng);
 
-  var currentDay = new Date().getDay();
-  console.log(currentDay);
-
+  //Place client's latititude and longitude coordinates inside Weather Unlocked API access link
   let link = 'http://api.weatherunlocked.com/api/forecast/' + req.body.lat + ',' + req.body.lng + '?app_id=48cb6b4b&app_key=c71f24d16dfd40152137a56d55e767d2'
-  console.log(link);
+
+  //Connect with Weather Unlocked API and receive weather forecast as JSON object
   request({
     url: link,
     json: true
   }, function (error, response, body) {
     if (!error && response.statusCode === 200) {
       let myObj = body
-      res.json(myObj);
+      res.json(myObj); //Send JSON to client
     }
   });
-  //res.render('index', {title: 'Visual Weather', message: 'POST'});
 });
 
--- public/js/script.js
+++ public/js/script.js
@@ -1,47 +1,47 @@
-const days = ['Sunay', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', "Saturday"];
-
-//const canvas = document.getElementById('canvas');
-//const ctx = canvas.getContext('2d');
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', "Saturday"];
 
 navigator.geolocation.getCurrentPosition(function(position) {
 
-	fetch('/', {
+
+	fetch('/', { //Get client's latitude and longitude coordinates, then send them to the server
 		method: 'POST',
 		body: JSON.stringify({ "lat": position.coords.latitude.toFixed(2), "lng": position.coords.longitude.toFixed(2), }),
 		headers: new Headers({ 'Content-Type': 'application/json' })
 	}).then(res => {
-		return res.json();
+		return res.json(); //Receive JSON object from server
 	}).catch (err => {
 		console.error('Error: ', error);
 	}).then(data => {
 		console.log(data);
 		var currentDay = new Date().getDay() + 1;
 
-			var chartData = {
-	  			labels: ['Today', days[(currentDay++)%7], days[(currentDay++)%7], days[(currentDay++)%7], days[(currentDay++)%7]],
-	  			series: [
-	    			[data.Days[1].temp_max_f, data.Days[2].temp_max_f, data.Days[3].temp_max_f, data.Days[4].temp_max_f, data.Days[5].temp_max_f],
-	    			[data.Days[1].temp_min_f, data.Days[2].temp_min_f, data.Days[3].temp_min_f, data.Days[4].temp_min_f, data.Days[5].temp_min_f]
-	  			]
-			};
+		//Prepare Chartist.js graph data
+		var chartData = {
+	 		labels: ['Today', days[(currentDay++)%7], days[(currentDay++)%7], days[(currentDay++)%7], days[(currentDay++)%7]],
+	 		series: [
+	    		[data.Days[1].temp_max_f, data.Days[2].temp_max_f, data.Days[3].temp_max_f, data.Days[4].temp_max_f, data.Days[5].temp_max_f],
+	    		[data.Days[1].temp_min_f, data.Days[2].temp_min_f, data.Days[3].temp_min_f, data.Days[4].temp_min_f, data.Days[5].temp_min_f]
+	  		]
+		};
 
-			var options = {
-  				showPoint: true,
-  				lineSmooth: false,
-  				axisX: {
-   					showGrid: true,
-    				showLabel: true
- 				},
-  				axisY: {
-    				offset: 60,
-    				labelInterpolationFnc: function(value) {
-      					return value + '°F';
-    				}
-  				}
-			};			
+		//Prepare Chartist.js graph settings
+		var options = {
+  			showPoint: true,
+  			lineSmooth: false,
+  			axisX: {
+   				showGrid: true,
+    			showLabel: true
+ 			},
+  			axisY: {
+    			offset: 60,
+    			labelInterpolationFnc: function(value) {
+      				return value + '°F';
+    			}
+  			}
+		};			
 
 		document.getElementsByTagName('p')[0].style.display = 'none';
-		new Chartist.Line('.ct-chart', chartData, options);
+		new Chartist.Line('.ct-chart', chartData, options); //Generate graph using Chartist.js
 	});
 });
 
